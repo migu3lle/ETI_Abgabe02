53ffd62a545db1821224f7d25303da16deb2bada
Migu3lle <gundi.m@hotmail.com>
2018-01-13 17:44:03 +0100

    testTransitions() implemented for getTransitions method

--- src/ab2/impl/GUNDACKER_KOPALI/test/NFAImplTest.java
+++ src/ab2/impl/GUNDACKER_KOPALI/test/NFAImplTest.java
@@ -4,6 +4,7 @@ import ab2.impl.GUNDACKER_KOPALI.impl.NFAImpl;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -32,9 +33,32 @@ public class NFAImplTest {
         int initialState = 1;
         characters.add('a');
         characters.add('b');
+        characters.add('c');
+        characters.add('d');
         NFAImpl nfa = new NFAImpl(3, characters, acceptingStates, initialState);
+        nfa.setTransition(0,"a",1);
+        nfa.setTransition(1,"b",0);
+        nfa.setTransition(0,"b",1);
+        nfa.setTransition(0,"c",1);
+        nfa.setTransition(0,"d",1);
+        nfa.setTransition(1,"d",2);
         nfa.setTransition(1,"a",2);
-        nfa.setTransition(2,"b",1);
-        System.out.println(nfa.getTransitions());
+        nfa.setTransition(2,"b",0);
+        nfa.setTransition(2,"c",0);
+
+        Set<String>[][] returnSet = nfa.getTransitions();
+        /*
+        Object[] objArray;
+        for (int i = 0; i < returnSet.length; i++) {
+            for (int j = 0; j < returnSet[i].length; j++) {
+                objArray = returnSet[i][j].toArray();
+                for (int k = 0; k < objArray.length; k++) {
+                    System.out.println(objArray[k]);
+                }
+            }
+        }
+        */
+        System.out.println("Test: " + returnSet[1][2].toString());
+        //System.out.println(returnSet[1][0].toString());
     }
 }
